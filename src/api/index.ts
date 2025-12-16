82fa9bef147bf05b5d6a66d2cddbea70bf6241e4
ziron02 <riman121@mail.ru>
2025-12-16 22:19:13 +0700

    ..

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,8 +10,8 @@ type RESPONSE_DATA = {
 const getJson = async <T>(endpoint: ENDPOINTS): Promise<T> => {
 	const path =
 		process.env.NODE_ENV === 'development'
-			? 'http://localhost:3001/api/${endpoint}'
-			: 'https://raw.githubusercontent.com/ziron02/react_app/gh-pages/static/db/${endpoint}.json'
+			? 'http://localhost:3001/api/' + endpoint
+			: `https://raw.githubusercontent.com/ziron02/react_app/gh-pages/static/db/${endpoint}.json`
 	const response = await fetch(path)
 
 	return await response.json()
